807253ee6f2a390c0ea2c6ae34ef3b4f7c75d18b
Laura <lhernan53112@gmail.com>
2021-02-07 13:39:00 -0500

    Added event listener that saves to db

--- src/main/java/com/hcl/user_feedback/controller/feedbackContoller.java
+++ src/main/java/com/hcl/user_feedback/controller/feedbackContoller.java
@@ -1,14 +1,28 @@
 package com.hcl.user_feedback.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.hcl.user_feedback.model.CrudRepo;
+import com.hcl.user_feedback.model.Feedback;
+
 @RestController
 public class feedbackContoller {
+	@Autowired
+	CrudRepo feedbackCrudRepo;
+	
 	@GetMapping("/")
-	public ModelAndView index() {
+	public ModelAndView showHomePage() {
 		return new ModelAndView("index.html");
+	}
+	
+	@PostMapping("/usersfeedback")
+	public Feedback saveUsersFeedback(@RequestBody Feedback feedback) {
+		return feedbackCrudRepo.save(feedback);	
 	}
 
 }
