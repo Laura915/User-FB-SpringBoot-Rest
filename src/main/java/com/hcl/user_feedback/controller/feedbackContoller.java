db4a613cc9525cb1fe0498136ade9360f1e7a168
Laura <lhernan53112@gmail.com>
2021-02-07 14:03:01 -0500

    Added thankyou screen

--- src/main/java/com/hcl/user_feedback/controller/feedbackContoller.java
+++ src/main/java/com/hcl/user_feedback/controller/feedbackContoller.java
@@ -1,5 +1,8 @@
 package com.hcl.user_feedback.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,6 +15,8 @@ import com.hcl.user_feedback.model.Feedback;
 
 @RestController
 public class feedbackContoller {
+	Logger logger = LoggerFactory.getLogger(feedbackContoller.class);
+	
 	@Autowired
 	CrudRepo feedbackCrudRepo;
 	
