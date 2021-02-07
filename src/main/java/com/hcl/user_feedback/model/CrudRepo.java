9ba06a88d7710e1a47a451bbce193e73d4d2828d
Laura <lhernan53112@gmail.com>
2021-02-07 11:41:29 -0500

    added Feedback table and added static html page

---
+++ src/main/java/com/hcl/user_feedback/model/CrudRepo.java
@@ -0,0 +1,7 @@
+package com.hcl.user_feedback.model;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CrudRepo extends CrudRepository<Feedback, Long>  {
+
+}
---
+++ src/main/java/com/hcl/user_feedback/model/Feedback.java
@@ -0,0 +1,40 @@
+package com.hcl.user_feedback.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Table()
+@Entity
+public class Feedback {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "id")
+	private Long id;
+
+	@Column(name = "name")
+	private String name;
+
+	@Column(name = "feedback")
+	private String feedback;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getFeedback() {
+		return feedback;
+	}
+
+	public void setFeedback(String feedback) {
+		this.feedback = feedback;
+	}
+	
+}
